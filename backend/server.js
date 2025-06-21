7646e3b77e746d616e70150551ebfd7116d49da2
viktorlypianets <lypianetsviktor@gmail.com>
2025-06-22 05:20:35 +0900

    change packa.json

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 require("dotenv").config();
 
 const app = express();
