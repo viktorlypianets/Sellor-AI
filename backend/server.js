260d82aa613322792a7f78e3df7a94a62aa25188
viktorlypianets <lypianetsviktor@gmail.com>
2025-06-22 05:59:56 +0900

    add crypt

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const path = require("path");
+const crypto = require("crypto");
 require("dotenv").config();
 
 const app = express();
