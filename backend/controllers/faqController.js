048ae01efdcbb883960a3bb6f10387bff64515f0
viktorlypianets <lypianetsviktor@gmail.com>
2025-07-01 01:35:57 +0900

    modify gpt3

--- backend/controllers/faqController.js
+++ backend/controllers/faqController.js
@@ -50,7 +50,7 @@ export const generateFAQs = async (productId, CusomterID) => {
     `;
 
     const completion = await openai.chat.completions.create({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "system",
